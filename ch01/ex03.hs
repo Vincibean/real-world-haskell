caf35d3ab32084db5d8ae7c1233c7ffec92674fa
Vincibean <andrebessi00@gmail.com>
2018-05-18 11:49:01 +0000

    added qsolution for ch01.ex03

--- ch01/ex03.hs
+++ ch01/ex03.hs
@@ -1,3 +1,3 @@
 -- The words function counts the number of words in a string. Modify the WC.hs example in order to count the number of words in a file.
 main = interact wordCount
-    where wordCount input = show (length (lines input)) ++ "\n"
+    where wordCount input = show (length (words input)) ++ "\n"
