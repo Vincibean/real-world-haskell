f38ec078eba94e1e03f6cfd5551314dcaef6f757
Vincibean <andrebessi00@gmail.com>
2018-05-19 22:01:48 +0000

    added solution for ch03.ex01

--- ch03/ex01.hs
+++ ch03/ex01.hs
@@ -1,1 +1,8 @@
 -- Write the converse of fromList for the List type: a function that takes a List a and generates a [a].
+
+data List a = Cons a (List a) | Nil deriving (Show)
+
+toList :: List a -> [a]
+toList Nil = []
+toList (Cons x xs) = x : toList xs
+
