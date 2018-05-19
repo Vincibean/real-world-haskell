268adb65a9fe0a290d24f9b3595029c72c5604eb
Vincibean <andrebessi00@gmail.com>
2018-05-19 22:23:49 +0000

    added solution for ch03.ex02

--- ch03/ex02.hs
+++ ch03/ex02.hs
@@ -1,2 +1,4 @@
 -- Define a tree type that has only one constructor, like our Java example. 
 -- Instead of the Empty constructor, use the Maybe type to refer to a node’s children.
+
+data Tree a = Node a (Maybe (Tree a)) (Maybe (Tree a)) deriving (Show)
