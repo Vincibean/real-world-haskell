e53d9cbf4ee316a4483916eb956d48181bf188a0
Vincibean <andrebessi00@gmail.com>
2018-05-19 23:20:39 +0000

    added solution for ch03.ex04 ; edited solution for ch03.ex06

--- ch03/ex06.hs
+++ ch03/ex06.hs
@@ -1,2 +1,4 @@
 -- Turn a list into a palindrome; i.e. it should read the same both backward and forward. 
 -- For example, given the list [1,2,3], your function should return [1,2,3,3,2,1].
+toPalindrome :: [a] -> [a]
+toPalindrome xs = xs ++ (reverse xs)
