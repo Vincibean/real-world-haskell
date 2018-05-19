ea57f3679d4cd62fc1834840576eb57ab9e00732
Andrea Bessi <andrebessi00@gmail.com>
2018-05-19 22:07:37 +0200

    created ex14 (no solution)

---
+++ ch03/ex14.hs
@@ -0,0 +1,3 @@
+-- Define a function that takes a list of two-dimensional points and computes the direction of each successive triple. 
+-- Given a list of points [a,b,c,d,e], it should begin by computing the turn made by [a,b,c], then the turn made by [b,c,d], then [c,d,e]. 
+-- Your function should return a list of Direction.
