c3eb0f8b8a6f55bd3d578552f2ab87e8ff9676ae
Vincibean <andrebessi00@gmail.com>
2018-05-19 23:32:02 +0000

    added solution for ch03.ex04 ; edited solution for ch03.ex08

--- ch03/ex08.hs
+++ ch03/ex08.hs
@@ -1,2 +1,6 @@
+import Data.List
 -- Create a function that sorts a list of lists based on the length of each sublist. 
 -- (You may want to look at the sortBy function from the Data.List module.)
+
+sortBySublistLength :: [[a]] -> [[a]]
+sortBySublistLength = sortBy (\a b -> compare (length a) (length b))
