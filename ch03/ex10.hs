ae841754fa95bc62d73eedc8b4e7e0945ce74a44
Andrea Bessi <andrebessi00@gmail.com>
2018-05-19 22:00:38 +0200

    created ex10 (no solution)

---
+++ ch03/ex10.hs
@@ -0,0 +1,12 @@
+-- The separator should appear between elements of the list, but it should not follow the last element. 
+-- Your function should behave as follows:
+-- 
+--   ghci> :load Intersperse 
+--   [1 of 1] Compiling Main        ( Intersperse.hs, interpreted )
+--   Ok, modules loaded: Main. 
+--   ghci> intersperse ',' [] 
+--   ""
+--   ghci> intersperse ',' ["foo"]
+--   "foo"
+--   ghci> intersperse ',' ["foo","bar","baz","quux"] 
+--   "foo,bar,baz,quux"
