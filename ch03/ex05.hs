f27eac203e1847edabb31803ae2dfab04605f2aa
Andrea Bessi <andrebessi00@gmail.com>
2018-05-19 21:43:04 +0200

    created ex05 (no solution)

---
+++ ch03/ex05.hs
@@ -0,0 +1,2 @@
+-- Write a function that computes the mean of a list, i.e. the sum of all elements in the list divided by its length. 
+-- (You may need to use the fromIntegral function to convert the length of the list from an integer into a floating-point number.)
