d096f1d1b5ad493e6d942b8a1664c8ff416ac2f4
Andrea Bessi <andrebessi00@gmail.com>
2018-05-19 22:03:41 +0200

    created ex11 (no solution)

---
+++ ch03/ex11.hs
@@ -0,0 +1,3 @@
+-- Using the binary tree type that we defined earlier in this chapter, write a function that will determine the height of the tree. 
+-- The height is the largest number of hops from the root to an Empty. 
+-- For example, the tree Empty has height zero; Node "x" Empty Empty has height one; Node "x" Empty (Node "y" Empty Empty) has height two; and so on.
