ce2303c34d54eca9557343b01002988e727dd531
Vincibean <andrebessi00@gmail.com>
2018-05-19 21:16:12 +0000

    added solution for ch02.ex03

--- ch02/ex03.hs
+++ ch02/ex03.hs
@@ -1,1 +1,14 @@
 -- Write a function, lastButOne, that returns the element before the last.
+lastButOne :: [a] -> a
+lastButOne [x, _] = x
+lastButOne (x: xs) = lastButOne xs
+
+lastButOne' :: [a] -> a
+lastButOne' xs = last (init xs)
+
+lastButOne'' :: [a] -> a
+lastButOne'' xs = last (take ((length xs) - 1) xs)
+
+
+lastButOne''' :: [a] -> a
+lastButOne''' = last . init
