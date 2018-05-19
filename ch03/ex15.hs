5add572a2adab61335d36537f08f4c02b8fff048
Andrea Bessi <andrebessi00@gmail.com>
2018-05-19 22:13:31 +0200

    created ex15 (no solution)

---
+++ ch03/ex15.hs
@@ -0,0 +1,3 @@
+-- Using the code from the preceding three exercises, implement Graham’s scan algorithm for the convex hull of a set of 2D points. 
+-- You can find good description of what a convex hull (http://en.wikipedia.org/wiki/Convex_hull) is, 
+-- and how the Graham scan algorithm (http://en.wikipedia.org/wiki/Graham_scan) should work, on Wikipedia (http://en.wikipedia.org/).
