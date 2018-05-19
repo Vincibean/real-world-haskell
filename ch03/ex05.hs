48064c9beaaf1780ce75721764571a517a4b54e7
Vincibean <andrebessi00@gmail.com>
2018-05-19 22:46:09 +0000

    added solution for ch03.ex04 ; edited solution for ch03.ex05

--- ch03/ex05.hs
+++ ch03/ex05.hs
@@ -1,2 +1,7 @@
 -- Write a function that computes the mean of a list, i.e. the sum of all elements in the list divided by its length. 
 -- (You may need to use the fromIntegral function to convert the length of the list from an integer into a floating-point number.)
+
+mean [] = 0
+mean xs = total / count
+  where total = sum xs
+        count = fromIntegral (length xs)
