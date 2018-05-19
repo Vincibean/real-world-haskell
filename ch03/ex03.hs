c13e69e74cb600d0a63108d1b5625a03d7bfac54
Vincibean <andrebessi00@gmail.com>
2018-05-19 22:35:57 +0000

    added solution for ch03.ex04 ; edited solution for ch03.ex03

--- ch03/ex03.hs
+++ ch03/ex03.hs
@@ -1,6 +1,5 @@
 -- Write a function that computes the number of elements in a list. 
 -- To test it, ensure that it gives the same answers as the standard length function.
 
-length' :: [a] -> Int
 length' [] = 0
 length' (_:xs) = 1 + length' xs
--- ch03/ex04.hs
+++ ch03/ex04.hs
@@ -1,1 +1,5 @@
 -- Add a type signature for your function to your source file. To test it,load the source file into ghci again.
+
+length' :: [a] -> Int
+length' [] = 0
+length' (_:xs) = 1 + length' xs
