a16b0eb797e78e24bd5c3a3159fc25b05a59cc79
Vincibean <andrebessi00@gmail.com>
2018-05-20 01:12:18 +0000

    added solution for ch03.ex04 ; edited solution for ch03.ex09

--- ch03/ex09.hs
+++ ch03/ex09.hs
@@ -1,3 +1,6 @@
 -- Define a function that joins a list of lists together using a separator value:
+
 intersperse :: a -> [[a]] -> [a]
-intersperse = undefined
+intersperse s xs = intersperseAcc [] xs
+  where intersperseAcc acc [] = acc
+        intersperseAcc acc (h:t) = intersperseAcc (acc ++ h ++ [s]) t
