8bfd6345b99cf51984999d24bd2334c86be0497d
Vincibean <andrebessi00@gmail.com>
2018-05-20 02:57:44 +0000

    added solution for ch03.ex14

--- ch03/Geometry.hs
+++ ch03/Geometry.hs
@@ -3,3 +3,10 @@ module Geometry where
 data Point2D = Point2D { x:: Double, y :: Double } deriving (Show, Ord, Eq)
 
 data Direction = Left | Right | Straight deriving (Show, Eq)
+
+turn :: Point2D -> Point2D -> Point2D -> Direction
+turn p1 p2 p3
+    | clockwise > 0 = Geometry.Right
+    | clockwise < 0 = Geometry.Left
+    | otherwise = Straight
+  where clockwise = (x p2 - x p1) * (y p3 - y p1) - (y p2 - y p1) * (x p3 - x p1)
--- ch03/ex13.hs
+++ ch03/ex13.hs
@@ -1,9 +1,4 @@
 import Geometry
 -- Write a function that calculates the turn made by three two-dimensional points and returns a Direction.
-
-turn :: Point2D -> Point2D -> Point2D -> Direction
-turn p1 p2 p3
-    | clockwise > 0 = Geometry.Right
-    | clockwise < 0 = Geometry.Left
-    | otherwise = Straight
-  where clockwise = (x p2 - x p1) * (y p3 - y p1) - (y p2 - y p1) * (x p3 - x p1)
+--
+-- See Geometry.hs (function turn)
