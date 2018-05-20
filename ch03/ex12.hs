870c7e63e82de0a4dfb5b2462a5b6661118fcdb2
Vincibean <andrebessi00@gmail.com>
2018-05-20 02:10:08 +0000

    added solution for ch03.ex12

--- ch03/ex12.hs
+++ ch03/ex12.hs
@@ -1,3 +1,5 @@
 -- Consider three two-dimensional points, a, b, and c. 
 -- If we look at the angle formed by the line segment from a to b and the line segment from b to c, it turns left, turns right, or forms a straight line. 
 -- Define a Direction data type that lets you represent these possibilities.
+
+data Direction = Left | Right | Straight deriving (Show, Eq)
