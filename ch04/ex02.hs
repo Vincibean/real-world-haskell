76c5ac6642aa4160e68e019968fd2eb08716848c
Andrea Bessi <andrebessi00@gmail.com>
2018-05-26 10:25:06 +0200

    Rename ex01.hs to ex02.hs

---
+++ ch04/ex02.hs
@@ -0,0 +1,5 @@
+-- Write a function splitWith that acts similarly to words but takes a predicate and a list of any type, 
+-- and then splits its input list on every element for which the predicate returns False:
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith = undefined
