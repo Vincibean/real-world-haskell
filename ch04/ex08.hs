c28c3c51d29bcdc1868f0168ad7c0c2a2ef749c5
Andrea Bessi <andrebessi00@gmail.com>
2018-05-26 10:55:23 +0200

    Create ex08.hs

---
+++ ch04/ex08.hs
@@ -0,0 +1,5 @@
+-- The Prelude function concat concatenates a list of lists into a single list and has the following type:
+--
+--    concat :: [[a]] -> [a]
+--
+-- Write your own definition of concat using foldr
