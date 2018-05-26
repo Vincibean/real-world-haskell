e9c60b58e520f038cee8bd3823dfc4e30bf113b8
Andrea Bessi <andrebessi00@gmail.com>
2018-05-26 10:30:59 +0200

    Create ex05.hs

---
+++ ch04/ex05.hs
@@ -0,0 +1,5 @@
+-- Use a fold (choosing the appropriate fold will make your code much simpler) to rewrite and improve upon the asInt function 
+-- from the earlier section“Explicit Recursion” on page 85.
+
+asInt_fold :: String -> Int
+asInt_fold = undefined
