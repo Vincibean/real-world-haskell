93ad5e22e08d796346e102bcd2a03f1f264d8abc
Andrea Bessi <andrebessi00@gmail.com>
2018-05-26 10:45:14 +0200

    Update ex05.hs

--- ch04/ex05.hs
+++ ch04/ex05.hs
@@ -1,5 +1,13 @@
 -- Use a fold (choosing the appropriate fold will make your code much simpler) to rewrite and improve upon the asInt function 
 -- from the earlier section“Explicit Recursion” on page 85.
+-- Your function should behave as follows:	
+-- 	
+--   ghci> asInt_fold "101"	
+--   101	
+--   ghci> asInt_fold "-31337"	
+--   -31337	
+--   ghci> asInt_fold "1798"	
+--   1798
 
 asInt_fold :: String -> Int
 asInt_fold = undefined
