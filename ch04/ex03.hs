57618ed8632c39391ebddf3bde05869cb60dc62a
Andrea Bessi <andrebessi00@gmail.com>
2018-05-27 23:50:30 +0200

    Update ex03.hs

--- ch04/ex03.hs
+++ ch04/ex03.hs
@@ -1,2 +1,18 @@
+import System.Environment (getArgs)
+
 -- Using the command framework from the earlier section “A Simple Command-Line Framework” on page 71, 
 -- write a program that prints the first word of each line of its input.
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction = id
