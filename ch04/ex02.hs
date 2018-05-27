928a9865612494e7aef89a1e93fbeb7937340bf8
Vincibean <andrebessi00@gmail.com>
2018-05-27 21:41:38 +0000

    added solution for ch04.ex02

--- ch04/ex02.hs
+++ ch04/ex02.hs
@@ -2,4 +2,9 @@
 -- and then splits its input list on every element for which the predicate returns False:
 
 splitWith :: (a -> Bool) -> [a] -> [[a]]
-splitWith = undefined
+splitWith _ [] = []
+splitWith p xs = (els ++ h): (splitWith p t)
+  where (els, rest) = span p xs
+        (h, t) = if null rest 
+                 then ([], []) 
+                 else ([head rest], tail rest)
