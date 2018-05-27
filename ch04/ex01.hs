0955a81d99d21aac5120682a9a02fb2b7a3d0c7d
Vincibean <andrebessi00@gmail.com>
2018-05-27 00:07:57 +0000

    added solution for ch04.ex01

--- ch04/ex01.hs
+++ ch04/ex01.hs
@@ -4,3 +4,38 @@
 --   safeTail :: [a] -> Maybe [a]
 --   safeLast :: [a] -> Maybe a
 --   safeInit :: [a] -> Maybe [a]
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+safeHead' :: [a] -> Maybe a
+safeHead' [] = Nothing
+safeHead' xs = Just (head xs)
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (_:xs) = Just xs
+
+safeTail' :: [a] -> Maybe [a]
+safeTail' [] = Nothing
+safeTail' xs = Just (tail xs)
+
+safeLast :: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast [x] = Just x
+safeLast (_:xs) = safeLast xs
+
+safeLast' :: [a] -> Maybe a
+safeLast' xs = safeHead (reverse xs)
+
+safeLast'' :: [a] -> Maybe a
+safeLast'' = safeHead . reverse
+
+safeLast''' :: [a] -> Maybe a
+safeLast''' [] = Nothing
+safeLast''' xs = Just (last xs)
+
+safeInit :: [a] -> Maybe [a]
+safeInit [] = Nothing
+safeInit xs = Just (init xs)
