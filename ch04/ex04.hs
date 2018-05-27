12a7b26373676264b9cb63ca57b8af9e6ca0810b
Andrea Bessi <andrebessi00@gmail.com>
2018-05-28 00:05:20 +0200

    Update ex04.hs

--- ch04/ex04.hs
+++ ch04/ex04.hs
@@ -1,1 +1,16 @@
+import System.Environment (getArgs)
+
 -- Write a program that transposes the text in a file. For instance, it should convert "hello\nworld\n" to "hw\neo\nlr\nll\nod\n".
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        myFunction = id
