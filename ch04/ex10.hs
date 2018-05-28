4a92846c592936fe7497f4de74fcbee2e80df06e
Vincibean <andrebessi00@gmail.com>
2018-05-28 08:07:26 +0000

    added solution for ch04.ex10

--- ch04/ex10.hs
+++ ch04/ex10.hs
@@ -3,3 +3,10 @@
 --    groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
 --
 -- Use ghci to load the Data.List module and figure out what groupBy does, then write your own implementation using a fold.
+
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy' f = foldr step [[]]
+  where step el ([]:t) = [el] : t
+        step el acc
+          | f el (head (head acc)) = (el : head acc) : (tail acc)
+          | otherwise = [el] : acc
