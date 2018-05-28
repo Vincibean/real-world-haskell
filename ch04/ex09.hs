08b2b018592a9b11930a2f46b92541a18266ab76
Vincibean <andrebessi00@gmail.com>
2018-05-28 07:09:35 +0000

    added solution for ch04.ex09

--- ch04/ex09.hs
+++ ch04/ex09.hs
@@ -1,1 +1,20 @@
 --  Write your own definition of the standard takeWhile function, first using explicit recursion, and then foldr.
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' _ [] = []
+takeWhile' p (h:t)
+  | p h = h : takeWhile' p t
+  | otherwise = []
+
+takeWhile'' :: (a -> Bool) -> [a] -> [a]
+takeWhile'' p xs = reverse (loop p [] xs)
+  where loop _ acc [] = acc
+        loop p acc (h:t)
+          | p h = loop p (h : acc) t
+          | otherwise = acc
+
+takeWhile''' :: (a -> Bool) -> [a] -> [a]
+takeWhile''' p xs = foldr step [] xs
+  where step el acc
+          | p el = el : acc
+          | otherwise = []
