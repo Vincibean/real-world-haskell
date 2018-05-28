6d93fb5ddef313474dfac37b201c1a3df76b51e5
Vincibean <andrebessi00@gmail.com>
2018-05-28 06:45:57 +0000

    added solution for ch04.ex07

--- ch04/ex07.hs
+++ ch04/ex07.hs
@@ -1,3 +1,5 @@
+import Data.Char (digitToInt, isDigit)  
+
 -- The asInt_fold function uses error, so its callers cannot handle errors. Rewrite the function to fix this problem:
 --
 --    ghci> asInt_either "33"
@@ -7,5 +9,14 @@
 
 type ErrorMessage = String
 
-asInt_either :: String -> Ei
-asInt_either = undefined
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either "" = Left "Empty String" 
+asInt_either str@(sign:num)
+    | null num && not (isDigit sign) = Left "Invalid Character"
+    | notAllDigits num = Left "We handle Integers only"
+    | tooLarge str = Left "too large a number"
+    | sign == '-' = Right (-1 * loop num)
+    | otherwise = Right (loop str)
+  where loop = foldl (\i c -> i * 10 + digitToInt c) 0
+        notAllDigits xs = not (all isDigit xs)
+        tooLarge s = (length s) >= (length (show (maxBound :: Int)))
