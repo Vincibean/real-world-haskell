779d435074524da29c5bddc4f028b588a4d13364
Vincibean <andrebessi00@gmail.com>
2018-05-28 06:40:30 +0000

    added solution for ch04.ex06

--- ch04/ex06.hs
+++ ch04/ex06.hs
@@ -1,3 +1,5 @@
+import Data.Char (digitToInt, isDigit)
+
 -- Extend your function to handle the following kinds of exceptional conditions by calling error:
 --
 --    ghci> asInt_fold ""
@@ -10,3 +12,15 @@
 --    *** Exception: Char.digitToInt: not a digit '.'
 --    ghci> asInt_fold "314159265358979323846"
 --    564616105916946374
+
+asInt_fold :: String -> Int
+asInt_fold "" = error "Empty String" 
+asInt_fold str@(sign:num)
+    | null num && not (isDigit sign) = error "Invalid Character"
+    | notAllDigits num = error "We handle Integers only"
+    | tooLarge str = error "too large a number"
+    | sign == '-' = -1 * loop num
+    | otherwise = loop str
+  where loop = foldl (\i c -> i * 10 + digitToInt c) 0
+        notAllDigits xs = not (all isDigit xs)
+        tooLarge s = (length s) >= (length (show (maxBound :: Int)))
