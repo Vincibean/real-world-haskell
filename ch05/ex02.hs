57c54bb4c129f77ade39470e0e6e3d121c123ace
Andrea Bessi <andrebessi00@gmail.com>
2018-06-03 17:48:15 +0200

    Create ex02.hs

---
+++ ch05/ex02.hs
@@ -0,0 +1,7 @@
+-- Our pretty printer does not take nesting into account. Whenever we open parentheses, braces, or brackets, 
+-- any lines that follow should be indented so that they are aligned with the opening character until a 
+-- matching closing character is encountered.
+-- Add support for nesting, with a controllable amount of indentation:
+
+fill :: Int -> Doc -> Doc
+fill = undefined
