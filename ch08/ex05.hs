5ec88e6c07bb210b7468c89a7ba221e0c2e7fc34
Andrea Bessi <andrebessi00@gmail.com>
2018-06-12 00:27:13 +0200

    Create ex05.hs

---
+++ ch08/ex05.hs
@@ -0,0 +1,4 @@
+-- The * wild card matches names only within a single directory. 
+-- Many shells have an extended wild card syntax, **, that matches names recursively in all directories. 
+-- For example, **.c would mean “match a name ending in .c in this directory or any subdirectory at any depth”. 
+-- Implement matching on ** wild cards.
