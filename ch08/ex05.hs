379fc9475272730823645644c9d8c29208bc7374
Vincibean <andrebessi00@gmail.com>
2018-06-26 07:59:19 +0000

    added TODO for ch08.ex05

--- ch08/ex05.hs
+++ ch08/ex05.hs
@@ -2,3 +2,5 @@
 -- Many shells have an extended wild card syntax, **, that matches names recursively in all directories. 
 -- For example, **.c would mean “match a name ending in .c in this directory or any subdirectory at any depth”. 
 -- Implement matching on ** wild cards.
+
+-- TODO unclear exercise: what should I do? Modify Glob.hs? Create a new function?
