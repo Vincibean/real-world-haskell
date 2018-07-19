dba4e4aa23029efe7aefa66bcfb4aab752ea15f1
Vincibean <andrebessi00@gmail.com>
2018-07-19 23:23:46 +0000

    added solution for ch08.ex08

--- ch08/ex08.hs
+++ ch08/ex08.hs
@@ -1,2 +1,25 @@
 -- Glob patterns are simple enough to interpret that it’s easy to write a matcher directly in Haskell, 
 -- rather than going through the regexp machinery. Give it a try.
+
+-- Yes, I know: it doesn't cover all the cases...
+
+type GlobPattern = String
+
+type Range = String
+
+matchGlob :: GlobPattern -> String -> Bool
+matchGlob "" "" = True
+matchGlob _ "" = False
+matchGlob "" _ = False
+matchGlob "*" _ = True
+matchGlob ('?' : ps) (_ : ss) = matchGlob ps ss
+matchGlob pat@('*' : ps) str@(s : ss) 
+  | ps == str = True 
+  | otherwise = matchGlob pat ss
+matchGlob ('[' : ps) (h : t) = let (range, pRest) = matchRange "" ps in if h `elem` range then matchGlob pRest t else False
+matchGlob x y = x == y
+
+matchRange :: String -> GlobPattern -> (Range, GlobPattern)
+matchRange acc "" = (acc, "")
+matchRange acc (']' : ps) = (acc, ps)
+matchRange acc (x : ps) = matchRange (x : acc) ps
