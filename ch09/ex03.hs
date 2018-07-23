6ba85acc68647da1c8fada688cff320ae6537e03
Vincibean <andrebessi00@gmail.com>
2018-07-23 08:10:46 +0000

    added solution for ch09.ex03

---
+++ ch09/ex03.hs
@@ -0,0 +1,22 @@
+-- Using id as a control function, traverse id performs a preorder traversal of a tree: it returns a parent directory before its children. Write a control function that makes traverse perform a postorder traversal, in which it returns children before their parent.
+
+import Data.List (sortBy)
+import Data.Ord (comparing)
+import ControlledVisit 
+import qualified ControlledVisit as P (traverse)
+
+postOrder :: [Info] -> [Info]
+postOrder = reverse
+
+postOrder' :: [Info] -> [Info]
+postOrder' = sortBy $ flip $ comparing infoSize
+
+-- Assumes that the parent directory will always be names "." and as such will be places after its content.
+postOrder'' :: [Info] -> [Info]
+postOrder'' = sortBy $ flip $ comparing infoPath
+
+traversePostOrder = P.traverse postOrder
+
+traversePostOrder' = P.traverse postOrder'
+
+traversePostOrder'' = P.traverse postOrder''
