01742647424523b91aeab80a81a17edd91def4d3
Vincibean <andrebessi00@gmail.com>
2018-08-08 21:58:13 +0000

    added solution for ch09.ex05

--- ch09/ex05.hs
+++ ch09/ex05.hs
@@ -1,4 +1,8 @@
 -- Write a wrapper for traverse that lets you control traversal using one predicate, and filter results using another.
 
+import ControlledVisit
+import qualified ControlledVisit as P (traverse)
 
-
+traverse' :: (Info -> Bool) -> (Info -> Bool) -> FilePath -> IO [Info]
+traverse' f t = P.traverse (filter (f .&&. t))
+  where (.&&.) f g a = f a && g a
