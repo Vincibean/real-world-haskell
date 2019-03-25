4acd1caf2e709ad93e14c15f0b6fb7e0cf264f08
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2019-03-25 14:20:42 -0700

    Update Geometry.hs

--- ch03/Geometry.hs
+++ ch03/Geometry.hs
@@ -6,10 +6,10 @@ data Direction = Left | Right | Straight deriving (Show, Eq)
 
 turn :: Point2D -> Point2D -> Point2D -> Direction
 turn p1 p2 p3
-    | clockwise > 0 = Geometry.Left
-    | clockwise < 0 = Geometry.Right
+    | determinant > 0 = Geometry.Left
+    | determinant < 0 = Geometry.Right
     | otherwise = Straight
-        where clockwise = (x p2 - x p1) * (y p3 - y p1) - (y p2 - y p1) * (x p3 - x p1)
+        where determinant = (x p2 - x p1) * (y p3 - y p1) - (y p2 - y p1) * (x p3 - x p1)
 
 turns :: [Point2D] -> [Direction]
 turns (p1 : p2 : p3 : ps) = turn p1 p2 p3 : turns (p2:p3:ps)
